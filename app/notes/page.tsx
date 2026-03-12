fa346106867a5be0d2252e47771f62f4a402cdb8
Wercche <lavellanc@gmail.com>
2026-03-12 11:51:38 +0100

    Add Supabase

---
+++ app/notes/page.tsx
@@ -0,0 +1,8 @@
+import { createSupabaseClient } from "@/lib/supabase/server";
+
+export default async function Notes() {
+  const supabase = await createSupabaseClient();
+  const { data: notes } = await supabase.from("notes").select();
+
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+}
